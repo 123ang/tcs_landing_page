16eed0354f197751aabf83ad0a5fe1ecf0b09dc7
choonben <choo_11@live.com>
2021-02-02 11:11:41 +0800

    2-2-2021

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,312 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <v-app>
+      <v-app-bar class="hidden-sm-and-down" app flat color="black" height="100spx">
+        <div class="mx-auto" style="height: 80px">
+          <v-img
+            max-height="80"
+            max-width="80"
+            src="./assets/logo1.jpg"
+            @click="$vuetify.goTo('#hero')"
+          ></v-img>
+        </div>
+
+        <div>
+          <v-toolbar-title>
+            <span class="md-toolbar-row"> </span>
+            <div style="position: absolute; right: 15%; text-transform: capitalize">
+              <v-btn
+                class="white--text"
+                text
+                style="text-transform: capitalize"
+                color="write"
+                height="40px"
+                @click="$vuetify.goTo('#about-us')"
+                >HOME</v-btn
+              >
+
+              <v-btn
+                class="white--text"
+                text
+                style="text-transform: capitalize"
+                color="write"
+                height="40px"
+                @click="$vuetify.goTo('#about-us')"
+                >ABOUT TCS</v-btn
+              >
+
+              <div class="dropdown">
+                <v-btn
+                  class="white--text"
+                  text
+                  style="text-transform: capitalize"
+                  color="write"
+                  height="40px"
+                  @click="$vuetify.goTo('#our-work')"
+                  >OUR COLLECTIONS <v-icon>{{ icons.mdiMenuDown }}</v-icon>
+                </v-btn>
+                <div class="dropdown-content">
+                  <a @click="$vuetify.goTo('#work1')">Sea Freight Services</a>
+                  <a @click="$vuetify.goTo('#work2')">Air Freight Services</a>
+                  <a @click="$vuetify.goTo('#work3')">Tracking &#38; Haulage Services</a>
+                  <a @click="$vuetify.goTo('#work4')">Warehouse &#38; Distribution</a>
+                  <a @click="$vuetify.goTo('#work5')">Fumigation Services</a>
+                  <a @click="$vuetify.goTo('#work6')">Dangerous / Hazardous Cargo</a>
+                </div>
+              </div>
+              <v-btn
+                class="white--text"
+                text
+                style="text-transform: capitalize"
+                color="write"
+                height="40px"
+                @click="$vuetify.goTo('#services')"
+                >EVENTS SHARING</v-btn
+              >
+
+              <v-btn
+                class="white--text"
+                text
+                color="write"
+                height="40px"
+                style="text-transform: capitalize"
+                @click="$vuetify.goTo('#contact')"
+                >CONTACT US</v-btn
+              >
+
+              <v-btn
+                class="btn"
+                rounded
+                style="background-color: black; color: #ffd600"
+                @click="$vuetify.goTo('#contact')"
+                >MEMBER</v-btn
+              >
+
+              <v-btn
+                class="btn"
+                rounded
+                style="background-color: black; color: #ffd600; left: 10px;"
+                @click="$vuetify.goTo('#contact')"
+                >REGISTER</v-btn
+              >
+
+              <v-btn
+                class="btn"
+                rounded
+                style="background-color: black; color: #ffd600; left: 20px;"
+                @click="$vuetify.goTo('#contact')"
+                >ESHOP</v-btn
+              >
+
+            </div>
+          </v-toolbar-title>
+        </div>
+      </v-app-bar>
+
+      <v-app-bar class="hidden-md-and-up" app flat color="white" height="70">
+        <v-toolbar color="white" height="70" flat>
+          <v-img
+            style="margin-left: -90px"
+            contain
+            max-height="60px"
+            max-width="200px"
+            src="./assets/logo1.jpg"
+            @click="$vuetify.goTo('#hero')"
+          ></v-img>
+          <v-app-bar-nav-icon
+            x-large
+            @click="drawer = !drawer"
+            color="write"
+            style="position: absolute; right: 5%"
+          ></v-app-bar-nav-icon>
+        </v-toolbar>
+        <v-navigation-drawer
+          v-model="drawer"
+          color="white "
+          absolute
+          temporary
+          width="100%"
+          :height="navHeight"
+        >
+          <v-toolbar color="white" height="70" flat>
+            <v-img
+              style="margin-left: -58px"
+              contain
+              max-height="60px"
+              max-width="200px"
+              src="./assets/logo1.jpg"
+              @click="$vuetify.goTo('#hero')"
+            ></v-img>
+
+            <v-icon
+              x-large
+              style="position: absolute; right: 7%"
+              @click="drawer = !drawer"
+              color="black"
+            >
+              {{ icons.mdiClose }}</v-icon
+            >
+          </v-toolbar>
+          <v-list style="text-align: center">
+            <v-btn
+              text
+              style="text-transform: capitalize"
+              color="yellow"
+              v-on:click.prevent="closeNav()"
+              height="40px"
+              @click="$vuetify.goTo('#about-us')"
+              >About Us</v-btn
+            >
+            <br />
+            <v-btn
+              text
+              style="text-transform: capitalize"
+              color="white"
+              v-on:click.prevent="closeNav()"
+              height="40px"
+              @click="$vuetify.goTo('#services')"
+              >Services</v-btn
+            >
+            <br />
+            <div class="dropdown">
+              <v-btn
+                white--text
+                outlined
+                style="text-transform: capitalize"
+                color="white"
+                height="40px"
+                v-on:click.prevent="toggleDrop()"
+                >Our Work <v-icon>{{ icons.mdiMenuDown }}</v-icon>
+              </v-btn>
+
+              <v-card flat v-if="drop" class="dropdown-content">
+                <a v-on:click.prevent="closeNav()" @click="$vuetify.goTo('#work1')"
+                  >Sea Freight Services</a
+                >
+                <a v-on:click.prevent="closeNav()" @click="$vuetify.goTo('#work2')"
+                  >Air Freight Services</a
+                >
+                <a v-on:click.prevent="closeNav()" @click="$vuetify.goTo('#work3')"
+                  >Tracking &#38; Haulage Services</a
+                >
+                <a v-on:click.prevent="closeNav()" @click="$vuetify.goTo('#work4')"
+                  >Warehouse &#38; Distribution</a
+                >
+                <a v-on:click.prevent="closeNav()" @click="$vuetify.goTo('#work5')"
+                  >Fumigation Services</a
+                >
+                <a v-on:click.prevent="closeNav()" @click="$vuetify.goTo('#work6')"
+                  >Dangerous / Hazardous Cargo</a
+                >
+              </v-card>
+            </div>
+            <br />
+            <v-btn
+              text
+              outlined
+              v-on:click.prevent="closeNav()"
+              color="white"
+              height="40px"
+              style="
+                background-color: rgb(10, 29, 65);
+                text-transform: capitalize;
+                margin-top: 0px;
+              "
+              @click="$vuetify.goTo('#contact')"
+              >Contact Us</v-btn
+            >
+
+            <v-btn class="black white-text">aa</v-btn>
+          </v-list>
+        </v-navigation-drawer>
+      </v-app-bar>
+
+      <v-content id="content-wrap">
+        <Home id="hero" />
+        <About id="about-us" />
+        <Services id="services" />
+        <OurWork id="our-work" />
+        <ContactUs id="contact" />
+        <Feedback id="feedback" />
+        <v-btn
+          v-scroll="onScroll"
+          v-show="fab"
+          class="scroll-top-btn"
+          fixed
+          bottom
+          right
+          dark
+          color="rgba(0, 0, 0, 0.2);"
+          @click="toTop"
+        >
+          <v-icon>{{ icons.mdiArrowUp }}</v-icon>
+        </v-btn>
+      </v-content>
+      <v-footer class="justify-center" color="#292929">
+        <div class="foot-text">
+          &copy; Copyright {{ new Date().getFullYear() }} Astraco Logistics Sdn. Bhd. All
+          Rights Reserved.
+        </div>
+      </v-footer>
+    </v-app>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { mdiMenuDown, mdiArrowUp, mdiClose } from "@mdi/js";
+import Home from "./views/Home";
+import About from "./views/About";
+import ContactUs from "./views/ContactUs";
+import Feedback from "./views/Feedback";
+import OurWork from "./views/OurWork";
+import Services from "./views/Services";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  components: { Home, About, ContactUs, Feedback, OurWork, Services },
+  data: () => ({
+    drawer: false,
+    icons: {
+      mdiMenuDown,
+      mdiClose,
+      mdiArrowUp,
+    },
+    drop: false,
+    navHeight: "250px",
+    fab: false,
+  }),
 
+  methods: {
+    toggleDrop() {
+      if (this.drop == true) {
+        this.navHeight = "250px";
+        this.drop = false;
+      } else if (this.drop == false) {
+        this.navHeight = "520px";
+        this.drop = true;
+      }
+    },
+    closeNav() {
+      this.drawer = false;
+      this.drop = false;
+      this.navHeight = "250px";
+    },
+    onScroll(e) {
+      if (typeof window === "undefined") return;
+      const top = window.pageYOffset || e.target.scrollTop || 0;
+      this.fab = top > 20;
+    },
+    toTop() {
+      this.$vuetify.goTo(0);
+    },
+  },
+};
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.btn {
+  border: 1px solid #ffd600;
+  background-color: black;
+  color: #ffd600;
+  font-size: 10px;
+  width: 106px;
+  height: 30px;
+  cursor: pointer;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,385 @@
+<template>
+  <div class="home">
+    <section>
+      <div>
+        <v-img src="../assets/background1.jpg">
+          <h1
+            class="company-name hidden-sm-and-down font_3"
+            style="font-size: 12px; color: white; margin-right: 500px"
+          >
+            Live Gold Price (per gram) :
+          </h1>
+          <div
+            class="iframeDiv"
+            style="font-size: 100px; color: white; margin-left: 300px; "
+          >
+            <iframe
+              class="iframe"
+              style="width: 70%;height:8%;background-color:orange;border:3px white solid;position: absolute;"
+              title="Table Master"
+              aria-label="Table Master"
+              scrolling="no"
+              src="https://wix-visual-data.appspot.com/app/widget?pageId=qlpgt&amp;compId=comp-k9i01vug&amp;viewerCompId=comp-k9i01vug&amp;siteRevision=226&amp;viewMode=site&amp;deviceType=desktop&amp;locale=en&amp;tz=Asia%2FKuala_Lumpur&amp;width=979&amp;height=77&amp;instance=o1iD_9tT0rHIXau_rRtcqrrXCE3-excvZgNa_PggWWg.eyJpbnN0YW5jZUlkIjoiMzVhZjQ0NjMtM2NmZi00ZWFkLWEzMWEtNjM2MjlkMTk0MDk3IiwiYXBwRGVmSWQiOiIxMzQxMzlmMy1mMmEwLTJjMmMtNjkzYy1lZDIyMTY1Y2ZkODQiLCJtZXRhU2l0ZUlkIjoiNWNiZGJkMmItYWQwMi00Mjc1LWFlMjEtMjdlNjdjYzA4MGJmIiwic2lnbkRhdGUiOiIyMDIxLTAyLTAxVDA1OjQyOjA1LjQzMVoiLCJkZW1vTW9kZSI6ZmFsc2UsImFpZCI6ImFmNWI1ZDE4LWNiOTEtNDNjYi05NzAxLTJiY2FmOGI4ZTZmOCIsImJpVG9rZW4iOiI2OTEyZjk0OC05MWZkLTBjZDgtMGQzYi00NDg0ZTFkOWMwMjgiLCJzaXRlT3duZXJJZCI6ImQ1NDg2MWZmLTIzMDEtNDA2Yi04MzY2LTgxZjVlOTQ5Yzk4OSJ9&amp;currency=MYR&amp;currentCurrency=MYR&amp;vsi=1ef378fc-8a81-439e-a6dd-3eba69981231&amp;commonConfig=%7B%22brand%22%3A%22wix%22%2C%22bsi%22%3A%2290224106-b24c-4bee-98f5-0014c712856d%7C1%22%2C%22BSI%22%3A%2290224106-b24c-4bee-98f5-0014c712856d%7C1%22%7D"
+              allowfullscreen=""
+              allowtransparency="true"
+              frameborder="0"
+              allow="autoplay;camera;microphone;geolocation;vr"
+            >
+            </iframe>
+          </div>
+          <h1
+            class="company-name hidden-sm-and-down font_3"
+            style="
+              font-size: 46px;
+              color: white;
+              font_family: NewsGothicBTW01-Italic;
+              font-weight: bold;
+            "
+          >
+            The most shiniest Diamond in the world
+          </h1>
+          <span class="hidden-md-and-up">
+            <br /><br /><br />
+            <h1 style="color: rgb(10, 29, 65)">ASTRACO GROUP OF COMPANIES</h1>
+          </span>
+        </v-img>
+      </div>
+    </section>
+    <section id="company-id">
+      <div>
+        <v-img height="300px" src="../assets/background2.jpg">
+          <v-card height="100%" color="rgb(0, 0, 0, 0.6)" align="center" justify="center">
+            <h4 class="company">
+              <br /><br />
+              <p class="company-p">ASTRACO FREIGHT FORWARDERS SDN. BHD. (143986-X)</p>
+              <p class="company-p">ASTRACO LOGISTICS SDN. BHD (489740-D)</p>
+              <p class="company-p">A.F.F SDN. BHD.</p>
+            </h4>
+            <br />
+            <h2 class="company">REVOLUTIONIZING LOGISTICS IN THE NEW</h2>
+            <br />
+            <v-btn
+              class="down-op-7"
+              color="grey darken-2"
+              fab
+              medium
+              dark
+              @click="$vuetify.goTo('#about-us')"
+            >
+              <v-icon
+                style="-webkit-animation: downarrow 0.6s infinite alternate ease-in-out"
+              >
+                mdi-arrow-down</v-icon
+              >
+            </v-btn>
+          </v-card>
+        </v-img>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import { mdiMenuDown, mdiArrowUp } from "@mdi/js";
+
+export default {
+  name: "Home",
+  data: () => ({
+    icons: {
+      mdiMenuDown,
+      mdiArrowUp,
+    },
+    fab: false,
+  }),
+  methods: {
+    onScroll(e) {
+      if (typeof window === "undefined") return;
+      const top = window.pageYOffset || e.target.scrollTop || 0;
+      this.fab = top > 20;
+    },
+    toTop() {
+      this.$vuetify.goTo(0);
+    },
+  },
+  created() {},
+};
+</script>
+
+<style>
+.scroll-top-btn {
+  height: 60px !important;
+  width: 50px !important;
+  opacity: 0.5;
+}
+#myBtn {
+  display: none;
+  position: fixed;
+  bottom: 20px;
+  right: 30px;
+  z-index: 99;
+  font-size: 18px;
+  border: none;
+  outline: none;
+  background-color: rgba(0, 0, 0, 0.2);
+  height: 60px;
+  width: 60px;
+  color: white;
+  cursor: pointer;
+  padding: 15px;
+  border-radius: 4px;
+}
+
+#myBtn:hover {
+  background-color: #07204c;
+}
+.foot-text {
+  font-family: "PT Sans Narrow", sans-serif;
+  font-size: 0.9rem;
+  font-style: normal;
+  line-height: 1.6;
+  word-break: break-word;
+  font-weight: 300;
+  color: white;
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+#route {
+}
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+/* Dropdown Content (Hidden by Default) */
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: white;
+  min-width: 160px;
+
+  z-index: 1;
+}
+
+/* Links inside the dropdown */
+.dropdown-content a {
+  color: black !important;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+  font-size: small;
+}
+
+/* Change color of dropdown links on hover */
+.dropdown-content a:hover {
+  background-color: #f1f1f1;
+}
+
+/* Show the dropdown menu on hover */
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
+/* Change the background color of the dropdown button when the dropdown content is shown */
+.flip-card-back a {
+  color: #07204c;
+  text-decoration: none;
+}
+.flip-card-back a:visited {
+  color: black;
+}
+.send-form-btn {
+  border-top-left-radius: 0;
+  border-bottom-left-radius: 0;
+  margin: 0.4rem 0;
+  position: relative;
+  border-radius: 0;
+  background: none;
+  border-color: white;
+  color: #ffffff;
+  font-weight: 500;
+  font-style: normal;
+  letter-spacing: 1px;
+  white-space: normal;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  word-break: break-word;
+  font-family: "PT Sans Narrow", sans-serif;
+  font-size: 0.9rem;
+  -webkit-appearance: none;
+  line-height: 1;
+  border: 2px solid;
+  padding: 20px 40px;
+}
+.lbl {
+  color: #ffffff;
+  font-family: "PT Sans Narrow", sans-serif;
+  font-size: 1.1rem;
+  display: inline-block;
+  touch-action: manipulation;
+}
+.office-title {
+  color: #07204c;
+  font-weight: 500;
+  font-family: "PT Sans Narrow", sans-serif;
+  font-size: 1.1rem;
+  padding-top: 1rem !important;
+  padding-bottom: 1rem !important;
+}
+.log-company {
+  color: #767676;
+  font-family: "PT Sans Narrow", sans-serif;
+  font-size: 1.1rem;
+  font-style: normal;
+  line-height: 1.6;
+}
+.map-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.mapouter {
+  position: relative;
+  text-align: center;
+  height: 500px;
+  width: 700px;
+}
+.gmap_canvas {
+  overflow: hidden;
+  background: none !important;
+  height: 500px;
+  width: 700px;
+}
+.work-black {
+  color: black;
+}
+.work-btn {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.work-title {
+  font-family: "Ubuntu Condensed", sans-serif;
+  font-size: 2.2rem;
+}
+.work-p {
+  font-family: "PT Sans Narrow", sans-serif;
+  font-size: 1rem;
+}
+.work-text {
+  color: white;
+  text-align: left;
+}
+.flip-card {
+  width: 300px;
+  height: 300px;
+  text-align: center;
+}
+
+/* This container is needed to position the front and back side */
+.flip-card-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+}
+
+/* Do an horizontal flip when you move the mouse over the flip box container */
+.flip-card:hover .flip-card-inner {
+  transform: rotateY(180deg);
+}
+
+/* Position the front and back side */
+.flip-card-front,
+.flip-card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden; /* Safari */
+  backface-visibility: hidden;
+}
+
+/* Style the front side (fallback if image is missing) */
+.flip-card-front {
+  color: black;
+}
+
+/* Style the back side */
+.flip-card-back {
+  background-color: white;
+  color: #07204c;
+  transform: rotateY(180deg);
+  padding: 5rem;
+  font-size: 10px;
+}
+.work {
+  background: #07204c;
+  color: white;
+  text-align: center;
+  font-family: "Ubuntu Condensed", sans-serif;
+  font-size: 1.5rem;
+  padding-top: 5%;
+  padding-bottom: 5%;
+}
+.service-title {
+  font-family: "PT Sans Narrow", sans-serif;
+  font-size: 1rem;
+  margin: 0;
+  padding: 2rem 2rem 0 2rem;
+}
+.service-text {
+  font-family: "PT Sans Narrow", sans-serif;
+  font-size: 0.9rem;
+  padding-bottom: 2rem;
+  color: white;
+}
+.about-title {
+  font-family: "Ubuntu Condensed", sans-serif;
+  font-size: 2.8rem;
+}
+.about-p {
+  color: #767676;
+  font-family: "PT Sans Narrow", sans-serif;
+  font-size: 1.1rem;
+}
+.down-op-7 {
+  opacity: 0.7;
+}
+.company-name {
+  text-align: center;
+  margin-top: 15%;
+  font-size: calc(60px + (26 - 14) * ((100vw - 300px) / (1600 - 300)));
+}
+.company {
+  text-align: center;
+  color: white;
+}
+.company-p {
+  line-height: 1;
+}
+
+.service-img {
+  padding: 1rem 20rem 5rem 5rem;
+  background: #07204c;
+  margin-top: 1rem;
+  height: 20%;
+}
+.other-service-img {
+  padding: 2rem 12rem 5rem 4rem;
+  margin-top: 1rem;
+  height: 20%;
+}
+
+@-webkit-keyframes downarrow {
+  0% {
+    -webkit-transform: translateY(0);
+    opacity: 0.8;
+  }
+  100% {
+    -webkit-transform: translateY(0.1em);
+    opacity: 0.9;
+  }
+}
+</style>
