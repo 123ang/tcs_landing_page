19878db11f12529ec11e1ada5dae64466b9ed0d0
choonben <choo_11@live.com>
2021-02-07 18:16:05 +0800

    7-2
    7-2

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,21 @@
        <div style="height: 100%;">
       <router-view />
       </div>
+      <Subscrip/>
      <Footer/>
+     
   </v-app>
 </template>
 <script>
 import Navbar from '@/views/Navbar'
 import Footer from '@/views/Footer'
+import Subscrip from '@/views/Subscrip'
 export default {
   name: 'App',
   components: {
       Navbar,
-      Footer
+      Footer,
+      Subscrip
   },
   data: () => ({
    
