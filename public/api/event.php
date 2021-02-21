0cfb187aee8bc6ccee3df8666458c60be764e4c0
choonben <choo_11@live.com>
2021-02-21 16:31:21 +0800

    v3
    v3

--- public/api/event.php
+++ public/api/event.php
@@ -1,17 +1,10 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
-include "config.php";
-date_default_timezone_set("Asia/Kuala_Lumpur");
 $con = new PDO("mysql:host=110.4.45.178;dbname=vmmsolut_ben;","vmmsolut_admin","123qwe!@#");
-mysqli_query($con, "SET character_set_client=utf8");
-mysqli_query($con, "SET character_set_connection=utf8");
 
 $receives_data= json_decode(file_get_contents("php://input"));	
  
 $data = array();
-if($receives_data->query == 'fetchall'){
+if($receives_data->action == 'fetchall'){
     $query = "SELECT * FROM event";
     $statement = $con->prepare($query);
     
