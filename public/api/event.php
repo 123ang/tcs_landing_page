c238292a5093b6f8ad1edf826a0ee3c8a4000758
choonben <choo_11@live.com>
2021-02-21 17:37:01 +0800

    v4
    v4

--- public/api/event.php
+++ public/api/event.php
@@ -1,13 +1,13 @@
 <?php
 $con = new PDO("mysql:host=110.4.45.178;dbname=vmmsolut_ben;","vmmsolut_admin","123qwe!@#");
 
-$receives_data= json_decode(file_get_contents("php://input"));	
+$received_data= json_decode(file_get_contents("php://input"));	
  
 $data = array();
-if($receives_data->action == 'fetchall'){
+if($received_data->actions == 'fetchall'){
     $query = "SELECT * FROM event";
     $statement = $con->prepare($query);
-    
+    $statement->execute();
     while($row = $statement->fetch(PDO::FETCH_ASSOC)){
         $data[] = $row;
     }
