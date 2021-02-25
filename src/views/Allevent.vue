f60ddfe1c4736e495e0ccbc18e18069f0145ed71
choonben <choo_11@live.com>
2021-02-25 21:54:11 +0800

    25-2
    25-2

--- src/views/Allevent.vue
+++ src/views/Allevent.vue
@@ -1,35 +1,66 @@
 <template>
   <div class="container">
-    <div v-for="row in allData" :key="row">
+<<<<<<< HEAD
+    <div  v-for="row in allData" :key="row" >
+      <v-container style="max-width: 1000px; ">
+        <v-row wrap >
+          <v-flex xs12 sm4 md4 p-4 style="margin:0px auto">
+            <div >
+              
+              <v-card
+                align="center"
+                justify="center"
+                max-width="300"
+                class="card"
+              ><p class="text-align: center;">{{ row.title }}</p>
+                <v-img contain v-bind:src="row.PathPic"> </v-img>
+                 <img src="" >
+              </v-card>
+            </div>
+          </v-flex>
+        </v-row>
+      </v-container>
+=======
+    <div v-for="row in allData" :key="row.id">
       {{ row.id }}
-     1
+     
+>>>>>>> 25da330b6fed15ffdd058f5c6e93c7123eb2dc0e
     </div>
   </div>
 </template>
+<style>
 
+</style>
 <script>
-
-import axios from 'axios'
+import axios from "axios";
 
 export default {
-  
-  data:() =>({ 
-     allData:''
-    
+  data: () => ({
+    allData: "",
   }),
-  methods: {
-    
-  },
-   created() {
-    
-      axios.post('https://ben.vmm-solution.my/php/event.php', { actions:'fetchall' }).then( response => {
+  methods: {},
+  created() {
+    axios
+      .post("https://ben.vmm-solution.my/php/event.php", {
+        actions: "fetchall",
+      })
+      .then((response) => {
         this.allData = response.data;
+<<<<<<< HEAD
         console.log(this.allData);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  },
+=======
+        console.log(response.data);
       }).catch(function (error) {
                     console.log(error);
                 });
     }
    
   
+>>>>>>> 25da330b6fed15ffdd058f5c6e93c7123eb2dc0e
 };
 </script>
