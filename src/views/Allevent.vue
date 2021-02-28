6c8944ba8569b3c1e6d4302dadf3579e7b552d52
choonben <choo_11@live.com>
2021-02-28 12:52:46 +0800

    21-2-2021

--- src/views/Allevent.vue
+++ src/views/Allevent.vue
@@ -1,50 +1,57 @@
 <template>
-  <div class="container">
-<<<<<<< HEAD
-<<<<<<< HEAD
-    <div  v-for="row in allData" :key="row" >
-      <v-container style="max-width: 1000px; ">
-        <v-row wrap >
-          <v-flex xs12 sm4 md4 p-4 style="margin:0px auto">
-            <div >
-              
-              <v-card
-                align="center"
-                justify="center"
-                max-width="300"
-                class="card"
-              ><p class="text-align: center;">{{ row.title }}</p>
-                <v-img contain v-bind:src="row.PathPic"> </v-img>
-                 <img src="" >
-              </v-card>
-            </div>
-          </v-flex>
-        </v-row>
-      </v-container>
-=======
-    <div v-for="row in allData" :key="row.id">
-      {{ row.id }}
-     
->>>>>>> 25da330b6fed15ffdd058f5c6e93c7123eb2dc0e
-=======
-    <div v-for="row in allData" :key="row.id">
-      {{ row.id }}
-     
->>>>>>> 25da330b6fed15ffdd058f5c6e93c7123eb2dc0e
-    </div>
-  </div>
+  <table style="background-color: black">
+    <p
+      style="
+        background-color: rgb(255, 214, 0);
+        font-size: 42px;
+        text-align: center;
+        font-family: didot-w01-italic, serif;
+      "
+    >
+      events sharing
+    </p>
+    <v-row wrap v-bind="index" v-for="(item, index) in listTemp" :key="index">
+      <v-flex xs12 sm4 md4 p-4 v-for="(cell, i) in item" :key="i">
+        <v-card elevation="9" style="relative">
+          <v-img contain v-bind:src="cell.PathPic"> </v-img>
+          <div >
+            <span
+              ><b>{{ cell.author }}</b></span
+            >
+          </div>
+          <p class="text-align: center;">{{ cell.title }}</p>
+        </v-card>
+      </v-flex>
+    </v-row>
+  </table>
 </template>
 <style>
-
 </style>
 <script>
 import axios from "axios";
 
 export default {
+  name: "event",
   data: () => ({
     allData: "",
   }),
   methods: {},
+  computed: {
+    listTemp: function () {
+      var list = this.allData;
+      var arrTemp = [];
+      var index = 0;
+      var sectionCount = 3;
+      for (var i = 0; i < list.length; i++) {
+        index = parseInt(i / sectionCount);
+        if (arrTemp.length <= index) {
+          arrTemp.push([]);
+        }
+        arrTemp[index].push(list[i]);
+      }
+      return arrTemp;
+    },
+  },
   created() {
     axios
       .post("https://ben.vmm-solution.my/php/event.php", {
@@ -52,24 +59,11 @@ export default {
       })
       .then((response) => {
         this.allData = response.data;
-<<<<<<< HEAD
-<<<<<<< HEAD
         console.log(this.allData);
       })
       .catch(function (error) {
         console.log(error);
       });
   },
-=======
-=======
->>>>>>> 25da330b6fed15ffdd058f5c6e93c7123eb2dc0e
-        console.log(response.data);
-      }).catch(function (error) {
-                    console.log(error);
-                });
-    }
-   
-  
->>>>>>> 25da330b6fed15ffdd058f5c6e93c7123eb2dc0e
 };
 </script>
