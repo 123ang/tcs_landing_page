7b7c496fdbe682b5852438fecbf247dfe76aca62
choonben <choo_11@live.com>
2021-03-04 17:53:57 +0800

    5.53

--- src/views/Allevent.vue
+++ src/views/Allevent.vue
@@ -1,29 +1,34 @@
 <template>
   <div class="w3-container" style="width: 100%">
-    <p
-      style="
-        background-color: rgb(255, 214, 0);
-        font-size: 42px;
-        text-align: center;
-        font-family: didot-w01-italic, serif;
-      "
-    >
-      events sharing
-    </p>
+    <div style="height:auto;">
+      <p
+        style="
+          background-color: rgb(255, 214, 0);
+          font-size: 42px;
+          text-align: center;
+          font-family: didot-w01-italic, serif;
+          
+        "
+      >
+        events sharing
+      </p>
+    </div>
 
-    <v-row wrap v-bind="index" v-for="(item, index) in listTemp" :key="index">
-      <v-flex xs12 sm4 md4 p-4 v-for="(cell, i) in item" :key="i">
-        <v-card elevation="9">
-          <v-img contain v-bind:src="cell.PathPic"> </v-img>
-          <div>
-            <span
-              ><b>{{ cell.author }}</b></span
-            >
-          </div>
-          <p class="text-align: center;">{{ cell.title }}</p>
-        </v-card>
-      </v-flex>
-    </v-row>
+    <table style="background-color: black">
+      <v-row wrap v-bind="index" v-for="(item, index) in listTemp" :key="index">
+        <v-flex xs12 sm4 md4 p-4 v-for="(cell, i) in item" :key="i">
+          <v-card elevation="9">
+            <v-img contain v-bind:src="cell.PathPic"> </v-img>
+            <div>
+              <span
+                ><b>{{ cell.author }}</b></span
+              >
+            </div>
+            <a onclick="func">{{ cell.title }}</a>
+          </v-card>
+        </v-flex>
+      </v-row>
+    </table>
   </div>
 </template>
 <script>
@@ -31,7 +36,7 @@ import axios from "axios";
 export default {
   name: "event",
   data: () => ({
-    allData: '',
+    allData: "",
   }),
   computed: {
     listTemp: function () {
@@ -40,7 +45,6 @@ export default {
       var index = 0;
       var sectionCount = 3;
       for (var i = 0; i < list.length; i++) {
-
         index = parseInt(i / sectionCount);
         if (arrTemp.length <= index) {
           arrTemp.push([]);
@@ -51,17 +55,23 @@ export default {
     },
   },
   methods: {
-          featchAllData: function () {
-            axios
-              .post("https://ben.vmm-solution.my/php/event.php", { actions: "fetchall" })
-              .then(response => this.allData = response.data)
-              .catch(error => {this.errorMessage = error.message;
-            console.error("There was an error!", error);
-          })},
+    featchAllData: function () {
+      axios
+        .post("https://ben.vmm-solution.my/php/event.php", { actions: "fetchall" })
+        .then((response) => (this.allData = response.data))
+        .catch((error) => {
+          this.errorMessage = error.message;
+          console.error("There was an error!", error);
+        });
+    },
+    func: function () {
+      var SchoolId = 0;
+      window.location.href = "school_info.aspx?edit_school=true&school=" + SchoolId;
+    },
   },
   created: function () {
     console.log(this.allData);
     this.featchAllData();
-  }
-}
+  },
+};
 </script>
